f5b629b0e2a90391967465f0b4035a3abb1a7ce1
stoney95 <simon.steinheber@maibornwolff.de>
2021-10-18 10:16:33 +0200

    refactor(examples): introduce Stage Enum

--- src/examples/arjan_codes_data_science_refactor/ds/tensorboard.py
+++ src/examples/arjan_codes_data_science_refactor/ds/tensorboard.py
@@ -4,35 +4,37 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 import numpy as np
 
+from .tracking import Stage
+
 
 def flush(writer: SummaryWriter):
     writer.flush()
 
 
-def add_batch_metric(writer: SummaryWriter, stage: str, name: str, value: float, step: int):
-    tag = f"{stage}/batch/{name}"
+def add_batch_metric(writer: SummaryWriter, stage: Stage, name: str, value: float, step: int):
+    tag = f"{stage.name}/batch/{name}"
     writer.add_scalar(tag, value, step)
 
 
-def add_epoch_metric(writer: SummaryWriter, stage: str, name: str, value: float, step: int):
-    tag = f"{stage}/epoch/{name}"
+def add_epoch_metric(writer: SummaryWriter, stage: Stage, name: str, value: float, step: int):
+    tag = f"{stage.name}/epoch/{name}"
     writer.add_scalar(tag, value, step)
 
 
-def add_epoch_confusion_matrix(writer: SummaryWriter, stage: str, y_true: list[np.ndarray], y_pred: list[np.ndarray], step: int):
+def add_epoch_confusion_matrix(writer: SummaryWriter, stage: Stage, y_true: list[np.ndarray], y_pred: list[np.ndarray], step: int):
     def _collapse_batch(batch_list: list[np.ndarray]) -> np.ndarray:
         return np.concatenate(batch_list)
 
     def _create_confusion_matrix(y_true: np.ndarray, y_pred: np.ndarray) -> plt.Figure:
         cm = ConfusionMatrixDisplay(confusion_matrix(y_true, y_pred))
-        cm.ax_.set_title(f"{stage} Epoch: {step}")
+        cm.ax_.set_title(f"{stage.name} Epoch: {step}")
         return cm
 
     y_true = _collapse_batch(y_true)
     y_pred = _collapse_batch(y_pred)
 
     fig = _create_confusion_matrix(y_true, y_pred)
-    tag = f"{stage}/epoch/confusion_matrix"
+    tag = f"{stage.name}/epoch/confusion_matrix"
     writer.add_figure(tag, fig, step)
 
 
--- src/examples/arjan_codes_data_science_refactor/ds/tracking.py
+++ src/examples/arjan_codes_data_science_refactor/ds/tracking.py
@@ -1,14 +1,21 @@
 from dataclasses import dataclass
 import numpy as np
-from typing import Callable, TypeVar, list, Generic
+from typing import Callable, TypeVar, List, Generic
+from enum import Enum, auto
 
 Writer = TypeVar("Writer")
+
+
+class Stage(Enum):
+    TRAIN = auto()
+    TEST = auto()
+    VAL = auto()
 
 
 @dataclass(frozen=True)
 class ExperimentTracker(Generic[Writer]):
     writer: Writer
     flush: Callable[[Writer], None]
-    add_batch_metric: Callable[[Writer, str, str, float, int], None]
-    add_epoch_metric: Callable[[Writer, str, str, float, int], None]
-    add_confusion_matrix: Callable[[Writer, str, list[np.ndarray], list[np.ndarray], int], None]
+    add_batch_metric: Callable[[Writer, Stage, str, float, int], None]
+    add_epoch_metric: Callable[[Writer, Stage, str, float, int], None]
+    add_confusion_matrix: Callable[[Writer, Stage, List[np.ndarray], List[np.ndarray], int], None]
