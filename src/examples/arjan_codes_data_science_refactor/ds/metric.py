6590e855d10a95a8f4adc2fed8b42c50d71dfc8d
stoney95 <simon.steinheber@maibornwolff.de>
2021-10-19 01:46:41 +0200

    refactor(examples): move accuracy to metric module

--- src/examples/arjan_codes_data_science_refactor/ds/metric.py
+++ src/examples/arjan_codes_data_science_refactor/ds/metric.py
@@ -1,22 +1,11 @@
-from dataclasses import dataclass, field
-from typing import list
+import numpy as np
+from sklearn.metrics import accuracy_score
+import torch
 
 
-@dataclass(frozen=True)
-class BatchMetric:
-    value: float
-    batch_size: int 
+def get_accuracy_score(pred: torch.Tensor, true: torch.Tensor) -> float:
+    y_np = true.detach().numpy()
+    y_prediction_np = np.argmax(pred.detach().numpy(), axis=1)
 
-
-@dataclass(frozen=True)
-class EpochMetric:
-    avg_value: float
-
-
-def batches_to_epoch(batches: list[BatchMetric]) -> EpochMetric:
-    total_value = sum([batch.values * batch.batch_size for batch in batches])
-    total_size = sum(map(lambda batch: batch.batch_size, batches))
-
-    avg = total_value / total_size
-    return EpochMetric(avg)
+    return accuracy_score(y_np, y_prediction_np)
 
