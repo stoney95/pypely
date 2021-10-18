5575f25c63154db8d4294959376e22e20b43415e
stoney95 <simon.steinheber@maibornwolff.de>
2021-10-19 01:48:10 +0200

    refactor(examples): add module for training dependencies

---
+++ src/examples/arjan_codes_data_science_refactor/ds/training.py
@@ -0,0 +1,12 @@
+import torch
+
+from dataclasses import dataclass
+from ds.tracking import ExperimentTracker
+
+
+@dataclass(frozen=True)
+class TrainingDependencies:
+    model: torch.nn.Module
+    optimizer: torch.optim.Optimizer
+    loss: torch.nn.modules.loss._Loss
+    experiment: ExperimentTracker
