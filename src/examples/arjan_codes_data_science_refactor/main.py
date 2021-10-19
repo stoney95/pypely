ce8b4fcd51682f876f739bc0ce3e798b1087d65b
stoney95 <simon.steinheber@maibornwolff.de>
2021-10-19 02:26:25 +0200

    refactor(examples): use new run levels in main

--- src/examples/arjan_codes_data_science_refactor/main.py
+++ src/examples/arjan_codes_data_science_refactor/main.py
@@ -1,16 +1,17 @@
-from pypely import pipeline, fork, to, identity
-from pypely.helpers import side_effect
+from pypely import pipeline, fork, to, identity, merge
+from pypely.helpers import side_effect, reduce_by
 
 import torch
+from typing import Any
 from torch.utils.tensorboard import SummaryWriter
 from pathlib import Path
-from dataclasses import asdict
 
 from ds.dataset import create_dataloader
-from ds.runner import run_epoch, Epoch
 from ds.models import LinearNet
-from ds.tracking import ExperimentTracker
-from ds.tensorboard import *
+from ds.tracking import ExperimentTracker, StageName
+from ds.training import TrainingDependencies
+from ds.run.epoch import run_epoch, EpochData, Epoch
+import ds.tensorboard as tb_tracking
 
 LR = 5e-5
 EPOCHS = 20
@@ -24,38 +25,71 @@ TEST_LABELS = DATA_DIR / "data/t10k-labels-idx1-ubyte.gz"
 
 
 create_model = pipeline(
-    lambda: LinearNet(), 
+    lambda: LinearNet(),
     fork(
         identity,
-        lambda model: torch.optim.Adam(model.parameters(), lr=LR),
+        lambda model: torch.optim.Adam(model.parameters(), lr=LR)
     )
 )
 
-
-#TODO: move this to runner.run_epoch
-flush_writer = lambda epoch: epoch.experiment.flush(epoch.experiment.writer)
-print_summary = lambda epoch: print(f"\nEpoch: {epoch.id}/{EPOCHS}, Test Accuracy: {epoch.test_accuracy: 0.4f}, Train Accuracy: {epoch.train_accuracy: 0.4}\n")
-
-epoch_summary = pipeline(
-    run_epoch,
-    side_effect(print_summary),
-    flush_writer
+create_training_dependencies = pipeline(
+    fork(
+        create_model,
+        lambda: torch.nn.CrossEntropyLoss(reduction="mean"),
+        lambda: ExperimentTracker[SummaryWriter](SummaryWriter(), tb_tracking.flush, tb_tracking.add_batch_metric, tb_tracking.add_epoch_metric, tb_tracking.add_epoch_confusion_matrix),
+    ),
+    to(TrainingDependencies)
 )
 
-run_epochs = lambda epoch: [epoch_summary(Epoch(**asdict(epoch), id=i)) for i in range(EPOCHS)]
+
+create_epoch_data = pipeline(
+    fork(
+        lambda: create_dataloader(BATCH_SIZE, TRAIN_DATA, TRAIN_LABELS),
+        lambda: create_dataloader(BATCH_SIZE, TEST_DATA, TEST_LABELS),
+    ),
+    to(EpochData)
+)
+
+
+def print_summary(epoch: Epoch, epoch_id: int, *_: Any) -> None:
+    summary = ", ".join(
+        [
+            f"[Epoch: {epoch_id + 1}/{EPOCHS}]",
+            f"Test Accuracy: {epoch.test.metric.accuracy: 0.4f}",
+            f"Train Accuracy: {epoch.train.metric.accuracy: 0.4f}",
+        ]
+    )
+    print("\n" + summary + "\n")
+
+
+def log_epoch(epoch: Epoch, i: int, training_dependencies: TrainingDependencies):
+    experiment = training_dependencies.experiment
+
+    experiment.add_epoch_metric(StageName.TRAIN, "accuracy", epoch.train.metric.accuracy, i)
+    experiment.add_epoch_metric(StageName.TEST, "accuracy", epoch.test.metric.accuracy, i)
+    experiment.add_confusion_matrix(StageName.TEST, epoch.test.result.true, epoch.test.result.pred, i)
+
+    experiment.flush()
+
+#TODO: increase readability with iterator
+epoch = pipeline(
+    fork(
+        reduce_by(run_epoch),
+        lambda training_dependencies, *_: training_dependencies, 
+    ),
+    side_effect(merge(log_epoch)),
+    side_effect(merge(print_summary)),
+)
+
+epochs = lambda training_dependencies, epoch_data: [epoch(training_dependencies, epoch_data, i) for i in range(EPOCHS)]
 
 main = pipeline(
     fork(
-        lambda: ExperimentTracker[SummaryWriter](SummaryWriter(), flush, add_batch_metric, add_epoch_metric, add_epoch_confusion_matrix),
-        lambda: create_dataloader(BATCH_SIZE, TRAIN_DATA, TRAIN_LABELS),
-        lambda: create_dataloader(BATCH_SIZE, TEST_DATA, TEST_LABELS),
-        lambda: create_model,
-        lambda: torch.nn.CrossEntropyLoss(reduction="mean")
+        create_training_dependencies,
+        create_epoch_data
     ),
-    to(Epoch, "experiment", "train_batches", "test_batches", "model", "optimizer", "loss_function"),
-    run_epochs
+    merge(epochs)
 )
-
 
 if __name__ == '__main__':
     main()
