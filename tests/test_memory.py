8f7be3c6bde39ee2f5b0e9f6d2a4a9ff068c734e
stoney95 <simon.steinheber@maibornwolff.de>
2021-10-25 18:47:19 +0200

    test(memory): add tests for the memory feature

---
+++ tests/test_memory.py
@@ -0,0 +1,46 @@
+from pypely.memory import add_to_memory, use_memory, PipelineMemory
+from pypely import pipeline, fork, merge
+
+import pytest
+
+def test_multiple_layers_access_fails(add, mul, sub):
+    
+
+    inner_pipe = pipeline(
+        with_memory_attribute("first_sum", mul),
+        with_memory_attribute("first_product", add)
+    )
+
+    to_test = pipeline(
+        fork(
+            add_to_memory("first_sum", add), 
+            add_to_memory("first_product", mul), 
+        ),
+        merge(sub),
+        inner_pipe
+    )
+
+    with pytest.raises(AttributeError):
+        to_test(1,2)
+
+
+def test_memory_access(add, mul, sub):
+    test_function = pipeline(
+        fork(
+            add_to_memory("first_sum", add), 
+            add_to_memory("first_product", mul), 
+        ),
+        merge(sub),
+        with_memory_attribute("first_sum", mul),
+        with_memory_attribute("first_product", add)
+    )
+
+    to_test = test_function(1,2)
+
+    assert to_test == 5
+
+
+def with_memory_attribute(name, func):
+        def __inner(x, memory: PipelineMemory):
+            return func(x, memory.get(name))
+        return use_memory(__inner)
