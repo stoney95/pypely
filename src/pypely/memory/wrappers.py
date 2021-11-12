ae280fad5fee5225fead1215d15205a5ba87d4b7
stoney95 <simon.steinheber@maibornwolff.de>
2021-11-12 09:50:15 +0100

    feat(memory): add memorizable
    `memorizable` allows the user to use an infix like syntax for writing
    to the memory or accessing the memory
    `memorizable` can be used as decorator or normal wrapper
    ```python
    from pypely.memory import memorizable
    from pypely import pipeline
    
    @memorizable
    def my_first_func(*args, **kwargs):
        ...
    
    def my_second_func(*args, **kwargs):
        ...
    
    _my_second_func = memorizable(my_second_func)
    
    pipeline(
        my_first_func >> "my_first_result",
        ...
        _my_second_func << "my_first_result"
    )
    ```

--- src/pypely/memory/wrappers.py
+++ src/pypely/memory/wrappers.py
@@ -1,10 +1,35 @@
 from pypely.memory._impl import get_memory, PipelineMemory
 from typing import Callable, TypeVar
+from functools import partial
 
 T = TypeVar("T")
 
 
-def add_to_memory(name: str, func: Callable[..., T]) -> Callable[..., T]:
+class memorizable:
+
+    def __init__(self, func):
+        self.func = func
+        self.attributes_after = []
+        self.attributes_before = []
+        
+    def __rshift__(self, other):
+        return add_to_memory(self.func, other)
+    def __lshift__(self, other):
+        self.attributes_after.append(other)
+        return self
+    def __rrshift__(self, other):
+        self.attributes_before.append(other)
+        return self
+    def __call__(self, *args):
+        memory = get_memory()
+        memory_attributes_before = [memory.get(attr) for attr in self.attributes_before]
+        memory_attributes_after = [memory.get(attr) for attr in self.attributes_after]
+        return self.func(*memory_attributes_before, *args, *memory_attributes_after)
+
+        # return with_memory_attribute(self.func, other)
+
+
+def add_to_memory(func: Callable[..., T], name: str, ) -> Callable[..., T]:
     def __inner(*args):
         result = func(*args)
         memory = get_memory()
@@ -21,7 +46,9 @@ def use_memory(func):
     return __inner
 
 
-def with_memory_attribute(name, func):
+def with_memory_attribute(func, name):
         def __inner(x, memory: PipelineMemory):
             return func(x, memory.get(name))
         return use_memory(__inner)
+
+
--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,18 +1,18 @@
-from pypely.memory import add_to_memory, use_memory, with_memory_attribute
+from pypely.memory import add_to_memory, with_memory_attribute, memorizable
 from pypely import pipeline, fork, merge
 
 import pytest
 
 def test_multiple_layers_access_fails(add, mul, sub):
     inner_pipe = pipeline(
-        with_memory_attribute("first_sum", mul),
-        with_memory_attribute("first_product", add)
+        with_memory_attribute(mul, "first_sum"),
+        with_memory_attribute(add, "first_product")
     )
 
     to_test = pipeline(
         fork(
-            add_to_memory("first_sum", add), 
-            add_to_memory("first_product", mul), 
+            add_to_memory(add, "first_sum"), 
+            add_to_memory(mul, "first_product"), 
         ),
         merge(sub),
         inner_pipe
@@ -25,12 +25,12 @@ def test_multiple_layers_access_fails(add, mul, sub):
 def test_memory_access(add, mul, sub):
     test_function = pipeline(
         fork(
-            add_to_memory("first_sum", add), 
-            add_to_memory("first_product", mul), 
+            add_to_memory(add, "first_sum"), 
+            add_to_memory(mul, "first_product"), 
         ),
         merge(sub),
-        with_memory_attribute("first_sum", mul),
-        with_memory_attribute("first_product", add)
+        with_memory_attribute(mul, "first_sum"),
+        with_memory_attribute(add, "first_product")
     )
 
     to_test = test_function(1,2)
@@ -38,3 +38,25 @@ def test_memory_access(add, mul, sub):
     assert to_test == 5
 
 
+def test_memorizable(add, mul, sub):
+    @memorizable
+    def sum_values(x, *args):
+        return x + sum(args)
+
+    _add = memorizable(add)
+    _mul = memorizable(mul)
+
+    test_function = pipeline(
+        fork(
+            _add >> "first_sum", 
+            _mul >> "first_product", 
+        ),
+        merge(sub),
+        _mul << "first_sum",
+        _add << "first_product",
+        "first_product" >> sum_values << "first_sum" << "first_product" << "first_sum"
+    )
+
+    to_test = test_function(1,2)
+
+    assert to_test == 15
