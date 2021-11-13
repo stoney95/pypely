1ed2f3d4897a6d646753a03b20dda7663c1f7434
stoney95 <simon.steinheber@maibornwolff.de>
2021-11-13 11:07:31 +0100

    test(memory): add error names and prohibited ingest

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,36 +1,43 @@
-from pypely.memory import add_to_memory, with_memory_attribute, memorizable
 from pypely import pipeline, fork, merge
+from pypely.memory import memorizable
+from pypely.memory.errors import *
 
 import pytest
 
 def test_multiple_layers_access_fails(add, mul, sub):
+    _mul = memorizable(mul)
+    _add = memorizable(add)
+
     inner_pipe = pipeline(
-        with_memory_attribute(mul, "first_sum"),
-        with_memory_attribute(add, "first_product")
+        _mul << "first_sum",
+        _add << "first_product"
     )
 
     to_test = pipeline(
         fork(
-            add_to_memory(add, "first_sum"), 
-            add_to_memory(mul, "first_product"), 
+            _add >> "first_sum",
+            _mul >> "first_product", 
         ),
         merge(sub),
         inner_pipe
     )
 
-    with pytest.raises(AttributeError):
+    with pytest.raises(MemoryAttributeNotFoundError):
         to_test(1,2)
 
 
 def test_memory_access(add, mul, sub):
+    _mul = memorizable(mul)
+    _add = memorizable(add)
+
     test_function = pipeline(
         fork(
-            add_to_memory(add, "first_sum"), 
-            add_to_memory(mul, "first_product"), 
+            _add >> "first_sum",
+            _mul >> "first_product",
         ),
         merge(sub),
-        with_memory_attribute(mul, "first_sum"),
-        with_memory_attribute(add, "first_product")
+        _mul << "first_sum",
+        _add << "first_product"
     )
 
     to_test = test_function(1,2)
@@ -40,8 +47,8 @@ def test_memory_access(add, mul, sub):
 
 def test_memorizable(add, mul, sub):
     @memorizable
-    def sum_values(x, *args):
-        return x + sum(args)
+    def sum_values(*args):
+        return sum(args)
 
     _add = memorizable(add)
     _mul = memorizable(mul)
@@ -60,3 +67,42 @@ def test_memorizable(add, mul, sub):
     to_test = test_function(1,2)
 
     assert to_test == 15
+
+
+def test_no_double_attr_assignment(add):
+    _add = memorizable(add)
+
+    to_test = fork(
+        _add >> "first_sum",
+        _add >> "first_sum"
+    )
+
+    with pytest.raises(MemoryAttributeExistsError):
+        to_test(1,2)
+
+
+def test_memorizable_as_normal_func(add):
+    _add = memorizable(add)
+
+    to_test = _add(1,2)
+    assert to_test == 3
+
+
+def test_memory_access_outside_pipeline(add):
+    _add = memorizable(add)
+
+    to_test = _add >> "test"
+    to_test(1,2)
+
+    try_access = _add << "test"
+    assert try_access(1) == 4
+
+
+def test_unallowed_memory_ingest(add):
+    _add = memorizable(add, allow_ingest=False)
+
+    with pytest.raises(MemoryIngestNotAllowedError):
+        pipeline(
+            _add >> "result",
+            _add << "result"
+        )
