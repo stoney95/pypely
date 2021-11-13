db7ba7b6607e9fd20aa3dfc8eabd6632325e356a
stoney95 <simon.steinheber@maibornwolff.de>
2021-11-13 11:06:24 +0100

    feat(memory): make it possible to prohibit memory ingest

--- src/pypely/memory/wrappers.py
+++ src/pypely/memory/wrappers.py
@@ -1,36 +1,43 @@
 from pypely.memory._impl import get_memory, PipelineMemory
+from pypely.memory.errors import MemoryIngestNotAllowedError
 from typing import Callable, TypeVar
-from functools import partial
 
 T = TypeVar("T")
 
 
-class memorizable:
+def memorizable(func=None, allow_ingest=True):
+    class Memorizable:
+        def __init__(self, func):
+            self.func = func
+            self.attributes_after = []
+            self.attributes_before = []
 
-    def __init__(self, func):
-        self.func = func
-        self.attributes_after = []
-        self.attributes_before = []
+        def __rshift__(self, other):
+            return _add_to_memory(self.func, other)
 
-    def __rshift__(self, other):
-        return add_to_memory(self.func, other)
+        def __lshift__(self, other):
+            if not allow_ingest:
+                raise MemoryIngestNotAllowedError(f"Memory ingest is not allowed for func: {self.func.__qualname__}")
+            self.attributes_after.append(other)
+            return self
 
-    def __lshift__(self, other):
-        self.attributes_after.append(other)
-        return self
+        def __rrshift__(self, other):
+            self.attributes_before.append(other)
+            return self
 
-    def __rrshift__(self, other):
-        self.attributes_before.append(other)
-        return self
+        def __call__(self, *args):
+            memory = get_memory()
+            memory_attributes_before = [memory.get(attr) for attr in self.attributes_before]
+            memory_attributes_after = [memory.get(attr) for attr in self.attributes_after]
+            return self.func(*memory_attributes_before, *args, *memory_attributes_after)
+    
+    if func is None:
+        return Memorizable
+    else:
+        return Memorizable(func)
 
-    def __call__(self, *args):
-        memory = get_memory()
-        memory_attributes_before = [memory.get(attr) for attr in self.attributes_before]
-        memory_attributes_after = [memory.get(attr) for attr in self.attributes_after]
-        return self.func(*memory_attributes_before, *args, *memory_attributes_after)
 
-
-def add_to_memory(func: Callable[..., T], name: str, ) -> Callable[..., T]:
+def _add_to_memory(func: Callable[..., T], name: str, ) -> Callable[..., T]:
     def __inner(*args):
         result = func(*args)
         memory = get_memory()
@@ -40,14 +47,14 @@ def add_to_memory(func: Callable[..., T], name: str, ) -> Callable[..., T]:
     return __inner
 
 
-def use_memory(func):
+def _use_memory(func):
     def __inner(*args):
         memory = get_memory()
         return func(*args, memory)
     return __inner
 
 
-def with_memory_attribute(func, name):
+def _with_memory_attribute(func, name):
         def __inner(x, memory: PipelineMemory):
             return func(x, memory.get(name))
-        return use_memory(__inner)
+        return _use_memory(__inner)
