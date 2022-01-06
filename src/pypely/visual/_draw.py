f34b5bf3f72e422231703b7e963aefe052a15c92
stoney95 <simon.steinheber@maibornwolff.de>
2022-01-06 17:24:52 +0100

    chore(draw): WIP: Add support for notebook rendering

--- src/pypely/visual/_draw.py
+++ src/pypely/visual/_draw.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
-from enum import Enum
-from typing import Callable, List, Tuple
-from pypely.components import decompose, Fork, Merge, Operation, Step, Pipeline
+from IPython.core.display import HTML, display
 from pathlib import Path
+from pypely.components import decompose, Fork, Merge, Operation, Step, Pipeline
+from typing import Callable, List, Optional, Tuple
 import tempfile
 import webbrowser
 import time
@@ -13,41 +13,45 @@ TEMPLATE_PATH = HERE / "template.html"
 
 
 
-def draw(pipeline: Callable, print_info: bool=False, path: Path=None, orientation: Orientation=None):
+def draw(pipeline: Callable, *, browser: bool=False, inline: bool=False, path: Path=None, print_info: bool=False) -> Optional[HTML]:
     components = decompose(pipeline) 
-    chart, edges, number_of_steps = _flowchart(components.steps, orientation=orientation)
+    chart, edges, number_of_steps = _flowchart(components.steps)
     html = _create_html(chart)
-
 
     if print_info:
         print(f"Found {len(edges)} edges: {edges}")
         print(f"Found {number_of_steps} steps")
 
-
     if path is not None:
-        _create_and_open(html, path.resolve())
-    else:
+        _create_and_open(html, path.resolve(), browser)
+    elif browser:
         with tempfile.TemporaryDirectory() as tmpdir:
-            path = Path(tmpdir) / "mermaid.html"
-            _create_and_open(html, path)
+            path = Path(tmpdir) / "pypely.html"
+            _create_and_open(html, path, browser)
             time.sleep(0.2)
 
+    if inline:
+        display(HTML(html))
 
-def _flowchart(steps: List[Step], chart: str=None, edges: List[Tuple[str, str]]=list(), prev_nodes: List[str]=None, i=0, level=1):
+
+def _flowchart(steps: List[Step], chart: str=None, edges: List[Tuple[str, str]]=None, prev_nodes: List[str]=None, i=0, level=1):
+    if edges is None:
+        edges = []
+
     tabs = " " * 2 * level
     if chart is None:
         chart = f"flowchart LR" 
-        chart += "\nclassDef pipeline fill:#A9FFFF,stroke:grey,stroke-width:1px;"
-        chart += "\nclassDef merge fill:#A5FFD6,stroke:grey,stroke-width:1px;"
-        chart += "\nclassDef fork fill:#90C8F9,stroke:grey,stroke-width:1px;"
         chart += "\nclassDef operation fill:#053C5E,color:white,stroke:#797B84,stroke-width:1px;"
+        chart += "\nclassDef pipeline fill:#a9ffffb3,stroke:grey,stroke-width:1px;"
+        chart += "\nclassDef fork fill:#90c8f9b3,stroke:grey,stroke-width:1px;"
+        chart += "\nclassDef merge fill:#a5ffd6b3,stroke:grey,stroke-width:1px;"
         chart += "\nsubgraph initialPipeline [ ]"
         chart += f"\ndirection LR"
 
     step = steps.pop(0)
     if type(step) is Operation:
         step_name = f"step{i}"
-        chart += f"\n{tabs}{step_name}([{step.func.__name__}])"
+        chart += f"\n{tabs}{step_name}([{_function_name(step.func)}])"
         chart += f"\n{tabs}class {step_name} operation"
         if prev_nodes is not None:
             for node in prev_nodes:
@@ -67,7 +71,7 @@ def _flowchart(steps: List[Step], chart: str=None, edges: List[Tuple[str, str]]=
     elif type(step) is Merge:
         step_name = f"step{i}"
         chart += f"\n{tabs}subgraph merge{i} [ ]"
-        chart += f"\n{tabs}{step_name}([{step.func.__name__}])"
+        chart += f"\n{tabs}{step_name}([{_function_name(step.func)}])"
         chart += f"\n{tabs}class {step_name} operation"
         chart += f"\n{tabs}end"
         chart += f"\n{tabs}class merge{i} merge"
@@ -94,6 +98,10 @@ def _flowchart(steps: List[Step], chart: str=None, edges: List[Tuple[str, str]]=
     else:
         return _flowchart(steps, chart, edges, added_nodes, i+1, level)
 
+    
+def _function_name(func):
+    return func.__name__.strip('<>_').replace('_', ' ')
+
 
 def _add_edges(chart: str, edges: List[Tuple[str, str]]) -> str:
     tabs = " " * 2
@@ -114,10 +122,12 @@ def _create_html(chart: str) -> str:
     return str(template)
 
 
-def _create_and_open(html: str, path: str=None):
+def _create_and_open(html: str, path: str=None, browser: bool=False):
     with open(path, 'w') as f:
         f.write(html)
-    _open_file_in_browser(path)
+    
+    if browser:
+        _open_file_in_browser(path)
 
     
 def _open_file_in_browser(html_file: str):
