948cb7d4aa0deb377674cee5478a88c5f104c6e8
Simon Steinheber <simon.steinheber@maibornwolff.de>
2023-03-04 15:51:47 +0100

    docs: add docstrings to `_internal` module

--- src/pypely/_internal/function_manipulation.py
+++ src/pypely/_internal/function_manipulation.py
@@ -1,11 +1,21 @@
-import inspect
-from typing import Any, Callable, TypeVar
-from typing_extensions import ParamSpec
+"""I enable the manipulation of a functions metadata.
 
+By this I enable the type checking during buildtime of a pipeline.
+`define_annotation` and `define_signature` are used to copy the annotation of a given function.
+
+This is especially useful to give the created pipeline the correct type. 
+Or check that the intermediate state of a created pipeline matches the next provided step typewise. 
+"""
+
+import inspect
 from copy import copy
+from typing import Any, Callable, TypeVar
+
+from typing_extensions import ParamSpec
 
 T = TypeVar("T")
 P = ParamSpec("P")
+
 
 def define_annotation(func: Callable, copy_parameters_from: Callable[P, Any], return_type: T) -> Callable[P, T]:
     """I reset the annotation of `func`.
@@ -43,9 +53,8 @@ def define_signature(func: Callable[P, T], copy_parameters_from: Callable[P, Any
         Callable[P, T]: The function with new signature
     """
     signature = inspect.Signature(
-        inspect.signature(copy_parameters_from).parameters.values(),
-        return_annotation=return_type
+        list(inspect.signature(copy_parameters_from).parameters.values()), return_annotation=return_type
     )
 
-    func.__signature__ = signature
+    setattr(func, "__signature__", signature)
     return func
