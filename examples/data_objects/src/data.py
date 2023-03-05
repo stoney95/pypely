eee84f6093654924609d21c189dbc9d6aceb325f
Simon Steinheber <simon.steinheber@maibornwolff.de>
2023-03-05 15:09:00 +0100

    docs: clean examples

--- examples/data_objects/src/data.py
+++ examples/data_objects/src/data.py
@@ -1,22 +1,32 @@
+"""I provide example data classes."""
+
 from dataclasses import dataclass, field
-from typing import List, Any
+from typing import Any, List
+
 
 @dataclass(frozen=True)
-class Me():
+class Me:
+    """noqa: D101."""
+
     position: str
     hungry: bool
     awake: bool = False
     preparing: Any = None
 
     def __repr__(self):
+        """noqa: D105.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         emotion = ""
         if self.awake:
             if self.hungry:
-                emotion ="😒"
+                emotion = "😒"
             else:
-                emotion ="😋"
+                emotion = "😋"
         else:
-            emotion ="😴"
+            emotion = "😴"
 
         __repr = f"{emotion} in the {self.position}"
         if self.preparing:
@@ -25,53 +35,113 @@ class Me():
 
 
 @dataclass(frozen=True)
-class Tea(): 
+class Tea:
+    """noqa: D101."""
+
     def __repr__(self):
+        """noqa: D105.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         return "🍵"
 
     def __str__(self):
+        """noqa: D105.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         return self.__repr__()
 
 
 @dataclass(frozen=True)
-class Bread():
+class Bread:
+    """noqa: D101."""
+
     def __repr__(self):
+        """noqa: D105.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         return "🍞"
-    
+
     def __str__(self):
+        """noqa: D105.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         return self.__repr__()
 
 
 @dataclass(frozen=True)
-class Eggs():
+class Eggs:
+    """noqa: D101."""
+
     def __repr__(self):
+        """noqa: D105.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         return "🍳"
 
     def __str__(self):
+        """noqa: D105.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         return self.__repr__()
 
 
 @dataclass(frozen=True)
-class Plate():
+class Plate:
+    """noqa: D101."""
+
     def __repr__(self):
+        """noqa: D105.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         return "🍽️"
 
     def __str__(self):
+        """noqa: D105.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         return self.__repr__()
 
+
 @dataclass(frozen=True)
-class Table():
+class Table:
+    """noqa: D101."""
+
     objects_on_table: List[Any] = field(default_factory=lambda: [])
 
     @property
     def is_set(self):
+        """noqa: D102.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         plate_on_table = any(type(obj) == Plate for obj in self.objects_on_table)
         at_least_3_elements = len(self.objects_on_table) >= 3
 
-        return (plate_on_table & at_least_3_elements)
+        return plate_on_table & at_least_3_elements
 
     def __repr__(self):
+        """noqa: D105.
+
+        # noqa: DAR101
+        # noqa: DAR201
+        """
         if self.is_set:
             return f"table set with: {' '.join([str(obj) for obj in self.objects_on_table])}"
         return "empty table"
-
